56bfd61b7713a8ec30f356bb3598b108900ff4dd
Josinaldo Jr <josinaldoeuzebiojr@gmail.com>
2023-08-08 21:07:57 -0300

    Update e3 and e5

--- exercicios-python/classes/e3.py
+++ exercicios-python/classes/e3.py
@@ -4,15 +4,15 @@ Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
 Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
 Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe as medidades de um local. Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés necessárias para o local.'''
 
-'''class Retangulo(object): 
+class Retangulo(object): 
     def __init__(self, base, altura):
         self.base = base 
         self.altura = altura    
    
-    def trocaLado(self):
+    def trocaBase(self, no):
         return "A Base do Retângulo agora é {} e a Altura é: {}".format(self.base, self.altura)       
 
-    def mostraLado(self):
+    def mostraLados(self):
         return self.base, "e", self.altura
     
     def mostraArea(self):
@@ -28,5 +28,5 @@ altura_local = float(input("Digite um valor para a Altura Local: "))
 base_piso = float(input("Digite um valor para a Base do piso: "))
 altura_piso = float(input("Digite um valor para a Altura do rodapé: "))
 
-retangulo_local = Retangulo(base_local, altura_local)'''
+retangulo_local = Retangulo(base_local, altura_local)
 # area_local = base_local * altura_local'''
